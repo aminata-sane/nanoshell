adb907c4af6da811aae9c8f02e5656850d974392
Linda Choucha <mehdi.choucha@laplateforme.io>
2026-01-16 14:54:12 +0100

    add comments for main.c

--- main.c
+++ main.c
@@ -1,34 +1,34 @@
 #include "shell_functions.h"
 
 int main() {
-    char buffer[1024];
-    char test_path[1024];
-    char cwd[1024];
+    char buffer[1024]; // store the command
+    char test_path[1024]; // find the executable in the files
+    char cwd[1024]; // store the path of the command
 
     while(1) {
-        display_prompt(cwd);
-        read_input(buffer);
+        display_prompt(cwd); // print the prompt
+        read_input(buffer); // read the user's command
 
-        char *buffer_copy = malloc(strlen(buffer) + 1);
-        strcpy(buffer_copy, buffer);
+        char *buffer_copy = malloc(strlen(buffer) + 1); // copy the buffer 
+        strcpy(buffer_copy, buffer); // stock it
 
-        char *command = strtok(buffer_copy, " ");
+        char *command = strtok(buffer_copy, " "); // cut the command
 
-        if (strcmp(buffer, "exit") == 0) {
-            free(buffer_copy);
+        if (strcmp(buffer, "exit") == 0) { // if exit is use 
+            free(buffer_copy); // free the space in buffer copy
             break;
         }
 
-        if (command != NULL && strcmp(command, "cd") == 0) {
-            char *argument = strtok(NULL, " ");
-            handle_cd(argument);
-            free(buffer_copy);
+        if (command != NULL && strcmp(command, "cd") == 0) { // is cd is use
+            char *argument = strtok(NULL, " "); // check the rest of the command for a destination folder
+            handle_cd(argument); // change the folder
+            free(buffer_copy); // free the space in buffer copy
             continue;
         }
 
-        resolve_path(buffer, test_path);
-        execute_command(buffer);
-        free(buffer_copy);
+        resolve_path(buffer, test_path); // find an absolute path
+        execute_command(buffer); // use the command in a child
+        free(buffer_copy); // free the space in buffer copy
     }
     return 0;
 }
